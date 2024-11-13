203db2a9f1bb41c20c2cbba4350211b400bc1688
Adam Mitchell <adam.mitchell@earthmill.co.uk>
2024-11-13 12:20:07 -0500

    adjust FT runner to test docker

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import os
 import time
 import unittest
 
@@ -15,6 +16,9 @@ MAX_WAIT = 5
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        test_server = os.environ.get("TEST_SERVER")
+        if test_server:
+            self.live_server_url = "http://" + test_server
 
     def tearDown(self):
         self.browser.quit()
