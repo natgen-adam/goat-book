bbef950686ae8e716b8ae57527cf1fcbfa57da25
Adam Mitchell <adam.mitchell@earthmill.co.uk>
2024-11-13 11:45:44 -0500

    use bootstrap for layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,7 @@
 import time
 import unittest
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,7 +12,7 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 5
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
