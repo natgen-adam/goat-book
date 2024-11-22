00fabd9c38197bb58ce90d9c756511783844d4d3
Adam Mitchell <adam.mitchell@earthmill.co.uk>
2024-11-22 15:56:11 -0500

    add token url to login link

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -2,6 +2,7 @@ from unittest import mock
 from django.test import TestCase
 
 import accounts.views
+from accounts.models import Token
 
 
 class SendLoginEmailViewTest(TestCase):
@@ -42,3 +43,20 @@ class LoginViewTest(TestCase):
     def test_redirects_to_home_page(self):
         response = self.client.get("/accounts/login?token=abcd123")
         self.assertRedirects(response, "/")
+
+    def test_creates_token_associated_with_email(self):
+        self.client.post(
+            "/accounts/send_login_email", data={"email": "edith@example.com"}
+        )
+        token = Token.objects.get()
+        self.assertEqual(token.email, "edith@example.com")
+
+    @mock.patch("accounts.views.send_mail")
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        self.client.post(
+            "/accounts/send_login_email", data={"email": "edith@example.com"}
+        )
+        token = Token.objects.get()
+        expected_url = f"http://testserver/accounts/login?token={token.uid}"
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
