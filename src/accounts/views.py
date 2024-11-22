bb89a3f1434af8be8a1507472ee06fd0734bbcce
Adam Mitchell <adam.mitchell@earthmill.co.uk>
2024-11-22 14:20:13 -0500

    add a mock test for check send_mail call args

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,5 +3,12 @@ from django.core.mail import send_mail
 
 
 def send_login_email(request):
+    email = request.POST["email"]
+    send_mail(
+        "Your login link for Superlists",
+        "body text tbc",
+        "noreply@superlists",
+        [email],
+    )
     return redirect("/")
 
