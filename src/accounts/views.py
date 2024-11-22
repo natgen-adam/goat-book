fa021ab7bec9c4fbf633faad7ab85326c398af0d
Adam Mitchell <adam.mitchell@earthmill.co.uk>
2024-11-22 14:30:33 -0500

    start of login view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
+from django.contrib import messages
 
 
 def send_login_email(request):
     email = request.POST["email"]
     send_mail(
         "Your login link for Superlists",
-        "body text tbc",
+        "Use this link to log in",
         "noreply@superlists",
         [email],
     )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in.",
+    )
     return redirect("/")
 
+def login(request):
+    return redirect("/")
