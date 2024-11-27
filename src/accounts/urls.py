d9bb8d51e3995aaa1f73863da275d1c5df45f0b9
Adam Mitchell <adam.mitchell@earthmill.co.uk>
2024-11-27 14:11:15 -0500

    logout using django auth_view

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from accounts import views
+from django.contrib.auth import views as auth_views
+
+from . import views
 
 urlpatterns = [
     path("send_login_email", views.send_login_email, name="send_login_email"),
     path("login", views.login, name="login"),
+    path("logout", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
 ]
