5d4e625d061d4710739ae6abb36ca11a1fac0472
Adam Mitchell <adam.mitchell@earthmill.co.uk>
2024-11-29 12:48:30 -0500

    test_my_lists: precreate sessions, move login checks to base

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -1,4 +1,4 @@
-from django.conf import setttings
+from django.conf import settings
 from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
 from django.contrib.sessions.backends.db import SessionStore
 
@@ -24,3 +24,13 @@ class MyListsTest(FunctionalTest):
                 path="/",
             )
         )
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = "edith@example.com"
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_out(email)
+
+        # Edith is a logged out user
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in(email)
